a9f26d8c38f2a59df1dbe78a5c9651851d115b12
Morten Bjørn <dev@mortenbjoern.com>
2022-02-12 21:57:20 +0100

    add footer component + carbon analysis

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,46 @@
+import { summary } from '../lib/summary';
+import Link from 'next/link';
+import Carbonbadge from '../lib/carbonbadge';
+
+// Import openSubmitOverlay from Context
+
 export default function Footer() {
   return (
-    <footer className="flex text-body-12 text-grey pt-2 items-center mt-25">
-      <div className="text-gray-500 space-y-2">
-        <p className="">Projekt af Morten Bjørn</p>
+    <footer className="flex flex-col md:flex-row md:flex-wrap md:w-full md:justify-between items-start mt-25">
+      <div className="w-2/3 md:w-35 space-y-2 md:space-y-3">
+        <p className="text-body-12 md:text-body-14">{summary}</p>
+        <button
+          onClick={() => openSubmitOverlay}
+          className="text-label-12 md:text-label-14 border p-1.25 md:p-1.5"
+        >
+          Submit position +
+        </button>
+      </div>
+      <nav className="mt-5 md:mt-0 space-y-2 flex flex-col text-label-12">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <a href="mailto:morten@nxtindx.com">Get in touch</a>
+        <Link href="/data-policy">Data policy</Link>
+      </nav>
+      <div className="mt-5 md:mt-7 space-y-0.5 text-body-12 md:w-full">
+        <p className="">
+          Project by{' '}
+          <a className="underline" href="https://twitter.com/mortenbjoern">
+            Morten Bjørn
+          </a>
+        </p>
+        <p className="">
+          Built by{' '}
+          <a
+            className="underline"
+            href="https://branch.climateaction.tech/issues/issue-1/designing-branch-sustainable-interaction-design-principles/"
+          >
+            sustainable interaction design principles
+          </a>
+        </p>
+      </div>
+      <div className="mt-0.75 text-label-12 space-x-1 flex flex-row justify-start">
+        <Carbonbadge />
       </div>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,7 @@ module.exports = {
       'label-24': ['24px', '24px'],
       'label-28': ['28px', '28px'],
       'label-32': ['32px', '32px'],
+      'label-40': ['40px', '40px'],
       'label-48': ['48px', '48px'],
       'body-12': ['12px', '16.8px'],
       'body-14': ['14px', '19.6px'],
